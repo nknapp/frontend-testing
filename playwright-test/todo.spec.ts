c06dba14754de021404a5448cab15cd626022dd6
Nils Knappmeier <nils.knappmeier@cosee.biz>
2025-04-25 21:22:10 +0200

    enterjs: add test for single HTML element

--- playwright-test/todo.spec.ts
+++ playwright-test/todo.spec.ts
@@ -2,6 +2,14 @@ import { test, expect } from "@playwright/test";
 
 test("has screenshot", async ({ page }) => {
   await page.goto("/");
-
   await expect(page).toHaveScreenshot();
+});
+
+test("todo has screenshot", async ({ page }) => {
+  await page.goto("/");
+  const el = page.getByTestId("todoItem").filter({
+    has: page.getByText("Get out of bed"),
+  });
+
+  await expect(el).toHaveScreenshot();
 });

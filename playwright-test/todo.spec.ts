d5bd9ed44d4f3e07891772fab16e10a2dacaa2ce
Nils Knappmeier <nils.knappmeier@cosee.biz>
2025-05-07 01:07:42 +0200

    prepare for enterJS 2025

--- playwright-test/todo.spec.ts
+++ playwright-test/todo.spec.ts
@@ -1,19 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { test, expect } from "@playwright/test";
-
 
 test("edit todo", async ({ page }) => {
   await page.goto("/", { waitUntil: "networkidle" });
-  await page.getByText("Get out of bed").click();
-  await page.getByLabel("Name").fill("Have breakfast");
-  await page.getByLabel("Description").fill("Sweet, sweet marmelade");
-  await page.click("text=Save");
-  await expect(page.getByText("Have breakfast")).toBeAttached()
-  await expect(page.getByText("Sweet, sweet marmelade")).toBeAttached()
-  await expect(page).toHaveScreenshot({
-    // acceptable perceived color difference in the YIQ color space
-    threshold: 0.2,
-
-    // number of pixels that may be different
-    maxDiffPixels: 0.1,
-  })
 });
